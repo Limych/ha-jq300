53f4187b7d7437deb44273e540f3596c519fb48e
Andrey Khrolenok <andrey@khrolenok.ru>
2020-04-18 18:41:33 +0300

    Add login function

--- custom_components/jq300/__init__.py
+++ custom_components/jq300/__init__.py
@@ -5,21 +5,37 @@ For more details about this component, please refer to
 https://github.com/Limych/ha-jq300
 """
 
+#
+#  Copyright (c) 2020, Andrey "Limych" Khrolenok <andrey@khrolenok.ru>
+#  Creative Commons BY-NC-SA 4.0 International Public License
+#  (see LICENSE.md or https://creativecommons.org/licenses/by-nc-sa/4.0/)
+#
+import json
 import logging
+from typing import Optional
 
+import homeassistant.helpers.config_validation as cv
+import requests
 import voluptuous as vol
+from homeassistant.const import CONF_USERNAME, CONF_PASSWORD, CONF_NAME
+from requests import Response, PreparedRequest
 
-from .const import DOMAIN, VERSION, ISSUE_URL, SUPPORT_LIB_URL, DATA_JQ300
+from .const import DOMAIN, VERSION, ISSUE_URL, SUPPORT_LIB_URL, DATA_JQ300, \
+    QUERY_TYPE_API, QUERY_TYPE_DEVICE, QUERY_METHOD_GET, BASE_URL_API, \
+    BASE_URL_DEVICE, USERAGENT_API, USERAGENT_DEVICE, QUERY_TIMEOUT, \
+    MSG_GENERIC_FAIL, MSG_LOGIN_FAIL
 
 _LOGGER = logging.getLogger(__name__)
 
+ACCOUNT_SCHEMA = vol.Schema({
+    vol.Required(CONF_USERNAME): cv.string,
+    vol.Required(CONF_PASSWORD): cv.string,
+    vol.Optional(CONF_NAME): cv.string,
+})
+
 CONFIG_SCHEMA = vol.Schema({
-    # DOMAIN: cv.schema_with_slug_keys(IAQ_SCHEMA),
+    DOMAIN: vol.All(cv.ensure_list, [ACCOUNT_SCHEMA])
 }, extra=vol.ALLOW_EXTRA)
-
-
-def _deslugify(string):
-    return string.replace('_', ' ').title()
 
 
 async def async_setup(hass, config):
@@ -29,6 +45,152 @@ async def async_setup(hass, config):
     _LOGGER.info('If you have ANY issues with this,'
                  ' please report them here: %s', ISSUE_URL)
 
-    # TODO
+    hass.data.setdefault(DATA_JQ300, {})
+
+    for index, account_config in enumerate(config[DOMAIN]):
+        username = account_config.get(CONF_USERNAME)
+        password = account_config.get(CONF_PASSWORD)
+        name = account_config.get(CONF_NAME, username)
+
+        if username in list(hass.data[DATA_JQ300]):
+            _LOGGER.error('Duplicate account! '
+                          'Account "%s" is already exists.', username)
+            continue
+
+        controller = JqController(hass, name, username, password)
+        hass.data[DATA_JQ300][username] = controller
+        _LOGGER.info('Connected to account "%s" as %s',
+                     controller.name, username)
+
+    if not hass.data[DATA_JQ300]:
+        return False
 
     return True
+
+
+class JqController:
+    """JQ device controller"""
+
+    def __init__(self, hass, name, username, password):
+        """Initialize configured device."""
+        self.hass = hass
+        self.params = {
+            'uid': -1000,
+            'safeToken': 'anonymous',
+        }
+
+        self._name = name
+        self._username = username
+        self._password = password
+
+        self._available = False
+        self._session = requests.session()
+
+        self._login()
+
+    @property
+    def unique_id(self):
+        """Return a device unique ID."""
+        return self._username
+
+    @property
+    def name(self):
+        """Get custom device name."""
+        return self._name
+
+    @property
+    def available(self) -> bool:
+        """Return True if device is available."""
+        # available = self._device.available
+        # if self._available != available:
+        #     self._available = available
+        #     _LOGGER.warning('Device "%s" is %s', self._name,
+        #                     'reconnected' if available else 'unavailable')
+        return self._available
+
+    @staticmethod
+    def _get_useragent(query_type) -> str:
+        """Generate User-Agent for requests"""
+        if query_type == QUERY_TYPE_API:
+            return USERAGENT_API
+        elif query_type == QUERY_TYPE_DEVICE:
+            return USERAGENT_DEVICE
+
+        raise ValueError('Unknown query type "%s"' % query_type)
+
+    def _add_url_params(self, url: str, extra_params: dict) -> str:
+        """Add params to URL."""
+        params = self.params.copy()
+        params.update(extra_params)
+
+        req = PreparedRequest()
+        req.prepare_url(url, params)
+
+        return req.url
+
+    def _get_url(self, query_type, function: str, extra_params=None) -> str:
+        """Generate request URL"""
+        if query_type == QUERY_TYPE_API:
+            url = BASE_URL_API + function
+        elif query_type == QUERY_TYPE_DEVICE:
+            url = BASE_URL_DEVICE + function
+        else:
+            raise ValueError('Unknown query type "%s"' % query_type)
+
+        if extra_params:
+            url = self._add_url_params(url, extra_params)
+        return url
+
+    def query(self, query_type, function: str, method=QUERY_METHOD_GET,
+              extra_params=None) -> Optional[Response]:
+        """Query data from cloud."""
+        url = self._get_url(query_type, function)
+        _LOGGER.debug("Querying %s %s", method.upper(), url)
+
+        response = None
+
+        # allow to override params when necessary
+        # and update self.params globally for the next connection
+        params = self.params.copy()
+        if query_type == QUERY_TYPE_DEVICE:
+            params['saveToken'] = params['safeToken']
+            del params['safeToken']
+        if extra_params:
+            params.update(extra_params)
+
+        try:
+            req = self._session.request(method, url, params=params, headers={
+                'User-Agent': self._get_useragent(query_type)
+            }, timeout=QUERY_TIMEOUT)
+            _LOGGER.debug("_query ret %s", req.status_code)
+
+        except Exception as err_msg:
+            _LOGGER.error("Error! %s", err_msg)
+            raise
+
+        if req.status_code == 200 or req.status_code == 204:
+            response = req
+
+        if response is None:  # pragma: no cover
+            _LOGGER.debug(MSG_GENERIC_FAIL)
+        return response
+
+    def _login(self):
+        ret = self.query(QUERY_TYPE_API, 'loginByEmail', extra_params={
+            'chr': 'clt',
+            'email': self._username,
+            'password': self._password,
+            'os': 2
+        })
+        if not ret:
+            return
+
+        ret = json.loads(ret.content)
+
+        if ret['code'] == 102:
+            _LOGGER.error(MSG_LOGIN_FAIL)
+        if ret['code'] != 2000:
+            _LOGGER.error(MSG_GENERIC_FAIL)
+
+        self.params['uid'] = ret['uid']
+        self.params['safeToken'] = ret['safeToken']
--- custom_components/jq300/const.py
+++ custom_components/jq300/const.py
@@ -5,6 +5,12 @@ For more details about this component, please refer to
 https://github.com/Limych/ha-jq300
 """
 
+#
+#  Copyright (c) 2020, Andrey "Limych" Khrolenok <andrey@khrolenok.ru>
+#  Creative Commons BY-NC-SA 4.0 International Public License
+#  (see LICENSE.md or https://creativecommons.org/licenses/by-nc-sa/4.0/)
+#
+
 # Base component constants
 DOMAIN = "jq300"
 VERSION = "0.1.0"
@@ -14,8 +20,23 @@ DATA_JQ300 = 'jq300'
 
 SUPPORT_LIB_URL = "https://github.com/Limych/jq300/issues/new/choose"
 
-_UA_SYSTEM = "Android 6.0.1; RedMi Note 5 Build/RB3N5C"
-UA_DALVIK = f"Dalvik/2.1.0 (Linux; U; {_UA_SYSTEM})"
-UA_MOZILLA = f"Mozilla/5.0 (Linux; {_UA_SYSTEM}; wv) AppleWebKit/537.36 " \
-             "(KHTML, like Gecko) Version/4.0 Chrome/68.0.3440.91 Mobile " \
-             "Safari/537.36"
+# Error strings
+MSG_GENERIC_FAIL = 'Sorry.. Something went wrong...'
+MSG_LOGIN_FAIL = 'Account name or password is wrong, please try again'
+
+QUERY_TYPE_API = 'API'
+QUERY_TYPE_DEVICE = 'DEVICE'
+
+QUERY_METHOD_GET = 'GET'
+QUERY_METHOD_POST = 'POST'
+
+BASE_URL_API = "http://www.youpinyuntai.com:32086/ypyt-api/api/app/"
+BASE_URL_DEVICE = "https://www.youpinyuntai.com:31447/device/"
+
+_USERAGENT_SYSTEM = "Android 6.0.1; RedMi Note 5 Build/RB3N5C"
+USERAGENT_API = f"Dalvik/2.1.0 (Linux; U; {_USERAGENT_SYSTEM})"
+USERAGENT_DEVICE = f"Mozilla/5.0 (Linux; {_USERAGENT_SYSTEM}; wv) " \
+                   "AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 " \
+                   "Chrome/68.0.3440.91 Mobile Safari/537.36"
+
+QUERY_TIMEOUT = 12
