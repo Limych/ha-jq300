36cfb465f807e70193f2d167983d0e3c5d38f0db
Andrey Khrolenok <andrey@khrolenok.ru>
2021-03-09 04:37:21 +0300

    Bump version

--- custom_components/jq300/const.py
+++ custom_components/jq300/const.py
@@ -32,7 +32,7 @@ from homeassistant.const import (
 # Base component constants
 NAME = "JQ-300/200/100 Indoor Air Quality Meter"
 DOMAIN = "jq300"
-VERSION = "0.8.0.dev0"
+VERSION = "0.8.0b0"
 ATTRIBUTION = "Data provided by JQ-300 Cloud"
 ISSUE_URL = "https://github.com/Limych/ha-jq300/issues"
 
